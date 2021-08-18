29982a040ab23f357b81dbd3a6b7acd52d051fdd
Carlos <carloseduardo2101@gmail.com>
2021-08-18 12:55:40 -0300

    Start of the project, couldn't make anything work ahahahaha

---
+++ Project-Trail-Hiker/Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D playerRb;
+    PlayerInputManager inputManager;
+
+    [SerializeField]
+    float speed = 0f;
+    [SerializeField]
+    float maxSpeed = 25f;
+    [SerializeField]
+    float decelerationVelocity = 5f;
+
+    int playerDirection = 1;
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        inputManager = GetComponent<PlayerInputManager>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+    }
+
+     public void FixedUpdate()
+     {
+        if (inputManager.wantsToMove)
+        {
+            Move();
+        } else
+        {
+            Stop();
+        }
+     }
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public void Move()
+    {
+        if(inputManager.walkInputAverageTime == 0 || playerRb.velocity.x > maxSpeed)
+        {
+            return;
+        }
+        playerRb.velocity = new Vector3(Time.fixedDeltaTime * playerDirection * speed / inputManager.walkInputAverageTime, playerRb.velocity.y, 0);
+    }
+
+    public void Stop()
+    {
+        //float newXVelocity = Mathf.Lerp(0, playerRb.velocity.x, decelerationRate);
+        //playerRb.velocity = new Vector3(newXVelocity, playerRb.velocity.y, 0);
+        if(playerRb.velocity.x > 1)
+        {
+            playerRb.velocity = new Vector3(playerRb.velocity.x + (-playerDirection * decelerationVelocity), playerRb.velocity.y, 0);
+        }
+    }
+
+     public void ChangePlayerDirection()
+     {
+         if(playerDirection == 1)
+         {
+             playerDirection = -1;
+         } else
+         {
+             playerDirection = 1;
+         }
+     }
+}
---
+++ Project-Trail-Hiker/Assets/Game/Scripts/PlayerInputManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    public bool aWasPressed { private set; get; } = false;
+    public bool dWasPressed { private set; get; } = false;
+    public float aDownTime { private set; get; } = 0.00f;
+    public float dDownTime { private set; get; } = 0.00f;
+    public float walkInputAverageTime { private set; get; } = 0.00f;
+    public bool wantsToMove { private set; get; }
+
+    public void Update()
+    {
+        Debug.Log("walkInputAverageTime = " + walkInputAverageTime);
+
+        if(aWasPressed && dWasPressed)
+        {
+            walkInputAverageTime = Mathf.Abs(aDownTime - dDownTime);
+        }
+        else if (aWasPressed)
+        {
+            walkInputAverageTime = Time.time - aDownTime;
+        }
+        else if (dWasPressed)
+        {
+            walkInputAverageTime = Time.time - dDownTime;
+        }
+
+        if(walkInputAverageTime > 1f)
+        {
+            wantsToMove = false;
+            walkInputAverageTime = 0f;
+        }
+        else
+        {
+            wantsToMove = true;
+        }
+
+        ProcessMovement();
+    }
+
+    public void ProcessMovement()
+    {
+        if (IsAButtonDown())
+        {
+            aDownTime = Time.time;
+            aWasPressed = true;
+            wantsToMove = true;
+        }
+
+        if (IsDButtonDown())
+        {
+            dDownTime = Time.time;
+            dWasPressed = true;
+            wantsToMove = true;
+        }
+
+        if (aWasPressed && dWasPressed)
+        {
+            Debug.Log("I'm Here!");
+            aWasPressed = false;
+            dWasPressed = false;
+            wantsToMove = false;
+        }
+    }
+
+    public bool IsAButtonDown()
+    {
+        return Input.GetKeyDown(KeyCode.A);
+    }
+
+    public bool IsDButtonDown()
+    {
+        return Input.GetKeyDown(KeyCode.D);
+    }
+
+    //public bool IsAButtonHold()
+    //{
+    //    return Input.GetKey(KeyCode.A);
+    //}
+
+    //public bool IsDButtonHold()
+    //{
+     //   return Input.GetKey(KeyCode.D);
+    //}
+
+    public bool IsSwipeDirectionButtonDown()
+    {
+        return Input.GetKeyDown(KeyCode.Q);
+    }
+}
